edfea136c6ba6ae637e69a9fd108c232ceffe8e2
Taylor Harper <tharper@int.dev>
2019-09-24 16:19:01 -0500

    * Fleshed out the profile application. * Added new table layout

--- tensor_host/profiles/urls.py
+++ tensor_host/profiles/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 
-from tensor_host.profiles.views import Profile
+from tensor_host.profiles.views import Profile, ProfileCreateForm, ProfileEditForm
 
 urlpatterns = [
-    path('', Profile.as_view())
+    path('', Profile.as_view(), name="profile"),
+    path('form/', ProfileCreateForm.as_view(), name="profile_create"),
+    path('form/<int:profile_id>/', ProfileEditForm.as_view(), name="profile_edit"),
 ]
--- tensor_host/profiles/views.py
+++ tensor_host/profiles/views.py
@@ -2,10 +2,37 @@ from django.shortcuts import render
 
 from django.views import View
 
+from profiles.models import ProfileCatalog, ProfileForm
+
 
 class Profile(View):
-    def get(self, request):
-        return render(request, 'profile/profile.html', {})
+    @staticmethod
+    def get(request):
+        profiles = ProfileCatalog.objects.all()
+        return render(request, 'profile/profile.html', {'profiles': profiles})
 
     def post(self, request):
+        pass
+
+
+class ProfileCreateForm(View):
+    @staticmethod
+    def get(request):
+        form = ProfileForm()
+        return render(request, 'profile/profile_form.html', {'form': form})
+
+    @staticmethod
+    def post(request):
+        pass
+
+
+class ProfileEditForm(View):
+    @staticmethod
+    def get(request, profile_id):
+        profile = ProfileCatalog.objects.get(id=profile_id)
+        form = ProfileForm(instance=profile)
+        return render(request, 'profile/profile_form.html', {'form': form, 'profile': profile})
+
+    @staticmethod
+    def post(request, profile_id):
         pass
