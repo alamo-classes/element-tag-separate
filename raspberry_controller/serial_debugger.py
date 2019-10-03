4d016844c0c393649507c3437f9c6366cbdc1220
Taylor Harper <taylor.harper@my.utsa.edu>
2019-10-02 19:31:44 -0500

    Removing debugging files

--- raspberry_controller/serial_debugger.py
+++
@@ -1,8 +0,0 @@
-import serial
-ard = serial.Serial("/dev/ttyACM0", 9600, timeout=5)
-user_input = ""
-while user_input is not "Z":
-    user_input = input("Enter the next character: ")
-    for cmd in list(user_input):
-        ard.write(bytes(cmd, 'UTF-8'))
-    print(ard.readline())
